ac6ea4658c0f3e1e26ce27b01fd0716b4ebdcde7
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-09-11 20:17:27 +0300

    change_structure_main/profile/sign-in/-up

---
+++ scripts/generative_posts.js
@@ -0,0 +1,80 @@
+
+
+
+
+const postsArr = [
+    {
+        id: 1,
+        name: 'marvel',
+        img_profile: '../images/rectangle/unsplash_IrQmtib-ER4.jpg',
+        img_post: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+        likes: 1390128,
+        description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+        comments: 6789,
+    },
+    {
+        id: 2,
+        name: 'asdasdasda',
+        img_profile: '../images/rectangle/unsplash_IrQmtib-ER4.jpg',
+        img_post: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+        likes: 139012123128,
+        description: 'nkjshzvkjlhfdslihvsdfkjv',
+        comments: 6123123789,
+    },
+    {
+        id: 3,
+        name: 'qweqweqeqw',
+        img_profile: '../images/rectangle/unsplash_IrQmtib-ER4.jpg',
+        img_post: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+        likes: 1328,
+        description: 'fsdgkldfjgkdjsl;fk;fdgjhjkjl;jlhgfjkljhgfjklhgfhjklhgfhjkhgfhjkhgfhjklhgfhjklhg',
+        comments: 69,
+    },
+]
+
+const main = document.querySelector('main');
+
+for (let i of postsArr) {
+    main.innerHTML += `
+<div class="post_block">
+    <div class="title_block">
+        <div class="profile">
+            <img src="${i.img_profile}" alt="" class="profile_img">
+            <h4>${i.name}</h4>
+        </div>
+        <button>
+            <img src="../images/icons/Group.svg" alt="">
+        </button>
+    </div>
+    <img src="${i.img_post}" alt="">
+    <div class="actions">
+        <div class="action_buttons">
+            <button>
+                <img src="../images/icons/Like.svg" alt="">
+            </button>
+            <button>
+                <img src="../images/icons/comment.svg" alt="">
+            </button>
+            <button>
+                <img src="../images/icons/send.svg" alt="">
+            </button>
+        </div>
+        <button>
+            <img src="../images/icons/bookmark.svg" alt="">
+        </button>
+    </div>
+    <div class="likes">
+        Liked by <span class="fiend">Pupa</span> and <span class="quantity">${i.likes} others</span>
+    </div>
+    <div class="description">
+        <span class="">${i.img_profile}</span>
+        ${i.description}
+    </div>
+    <button class="view_comments">view all <span class="quantity">${i.comments}</span> comments</button>
+    <div class="send_user_comment">
+        <img src="../images/rectangle/unsplash_Ve7xjKImd28.jpg" alt="" class="profile_img"> 
+        <textarea name="your_comment" id="" placeholder="Your comment"></textarea>
+        <button>Send</button>
+    </div>
+</div>`
+}
