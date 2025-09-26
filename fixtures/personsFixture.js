a9100a4b02e1956351dc7a489a6e6ca4d46118a3
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-09-26 15:22:00 +0300

    fix_create_stories_button

---
+++ fixtures/personsFixture.js
@@ -0,0 +1,107 @@
+const personsArr = [
+    {
+        id: 1,
+        phone: '+79932341256',
+        main: 'fsdsdfs@mail.ru',
+        name: 'Alexander',
+        avatar: '../images/rectangle/unsplash_S6hq9V3cfxQ.jpg',
+        login: 'alexander_lohpedovich228',
+        description: 'hakjhlgiak,hfdlg,hjfdbghjkbfhkjfbvjahkd',
+        links: ['https://www.google.com/?hl=ru'],
+        password: 'asdsakdahsdkjansdhajksd',
+        posts: [
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+            {
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+                likes: 1390128,
+                description: 'loremsfjklsdjfsdjflkadsjflkjsdalfkjasdklfjsalkdjfklsdjfklsajfkdljsakldfjslkdfjlksdjfklsjkldfjasdfklsjlkfjasdlfjkls',
+                comments: 6789,
+            },
+        ],
+        stories: [
+            {
+                description: 'summer',
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+            },
+            {
+                description: 'winter',
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+            },
+            {
+                description: 'mother',
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+            },
+            {
+                description: 'dog',
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+            },
+            {
+                description: 'live',
+                img: '../images/rectangle/unsplash_IMfJYJJZMUw.jpg',
+            },
+        ],
+        subscribers: 12321,
+        subscriptions: 324,
+    },
+];
+
+export default personsArr;
+// Как развернуть функционал чтобы можно было узнать истории и посты человека?
+
+
+
+
+
--- fixtures/storyFixture.js
+++ fixtures/storyFixture.js
@@ -1,3 +1,4 @@
+
 const storiesArr = [
     {
         id: 1,
@@ -7,47 +8,47 @@ const storiesArr = [
     {
         id: 2,
         name: 'wrwerwrw',
-        image: '../images/rectangle/unsplash_y2oAd10UfIU.jpg', 
+        image: '../images/rectangle/unsplash_9A72G2L8JuU.jpg', 
     },
     {
         id: 3,
         name: 'erterytreyrt',
-        image: '../images/rectangle/unsplash_y2oAd10UfIU.jpg', 
+        image: '../images/rectangle/default_avatar.png', 
     },
     {
         id: 4,
         name: 'rasretertist',
-        image: '../images/rectangle/unsplash_y2oAd10UfIU.jpg', 
+        image: '../images/rectangle/unsplash_JSXZnudXhXk.jpg', 
     },
     {
         id: 5,
         name: 'sdfsdf',
-        image: '../images/rectangle/unsplash_y2oAd10UfIU.jpg', 
+        image: '../images/rectangle/unsplash_IrQmtib-ER4.jpg', 
     },
     {
         id: 6,
         name: 'rasdfsdsist',
-        image: '../images/rectangle/unsplash_y2oAd10UfIU.jpg', 
+        image: '../images/rectangle/unsplash_agGIKYs4mYs.jpg', 
     },
     {
         id: 7,
         name: 'sdfsdfs',
-        image: '../images/rectangle/unsplash_y2oAd10UfIU.jpg', 
+        image: '../images/rectangle/unsplash_S6hq9V3cfxQ.jpg', 
     },
     {
         id: 8,
         name: 'fnklsdjf',
-        image: '../images/rectangle/unsplash_y2oAd10UfIU.jpg', 
+        image: '../images/rectangle/unsplash_BI91NrppE38.jpg', 
     },
     {
         id: 9,
         name: 'log',
-        image: '../images/rectangle/unsplash_y2oAd10UfIU.jpg', 
+        image: '../images/rectangle/unsplash_S6hq9V3cfxQ.jpg', 
     },
     {
         id: 10,
         name: 'log',
-        image: '../images/rectangle/unsplash_y2oAd10UfIU.jpg', 
+        image: '../images/rectangle/unsplash_BI91NrppE38.jpg', 
     },
     {
         id: 11,
